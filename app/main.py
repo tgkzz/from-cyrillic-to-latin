fa41a0517f6cbc773dab4eb9cdde712afcffe8f0
tgkzz <lilkamavertttt@gmail.com>
2023-10-19 17:54:03 +0600

    added error handling

--- app/main.py
+++ app/main.py
@@ -1,21 +1,44 @@
 from fastapi import FastAPI, Request, Form
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
+from pathlib import Path
+
 
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    name="static",
+)
+
 templates = Jinja2Templates(directory="templates")
+
+@app.exception_handler(404)
+async def http_not_found_handler(request, __):
+    return templates.TemplateResponse("error.html", {"request": request, "status_code": 404})
+
+
+@app.exception_handler(405)
+async def http_method_not_allowed_handler(request, __):
+    return templates.TemplateResponse("error.html", {"request": request, "status_code": 405})
+
 
 @app.get("/")
 def root(request: Request):
-    return templates.TemplateResponse("html/index.html", {"request": request, "translated_text": None})
+    return templates.TemplateResponse("index.html", {"request": request, "translated_text": None})
 
 @app.post("/result")
-def translate(request: Request, input_text: str = Form(...)):
+def translate(request: Request, input_text: str = Form(default="")):
     translated_text = cyrillic_to_latin(input_text)
-    return templates.TemplateResponse("html/index.html", {"request": request, "translated_text": translated_text})
+    return templates.TemplateResponse("index.html", {"request": request, "translated_text": translated_text})
 
 
 def cyrillic_to_latin(text):
+    if not bool(text):
+        return "text must be not empty"
+
     transliteration = {
         'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
         'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
