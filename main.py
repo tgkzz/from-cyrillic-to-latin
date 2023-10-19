e4d95bbb1fae571d9f407e248058e2bf0da99086
tgkzz <lilkamavertttt@gmail.com>
2023-10-19 16:45:44 +0600

    added normal backend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
@@ -10,28 +10,26 @@ def root(request: Request):
     return templates.TemplateResponse("html/index.html", {"request": request, "translated_text": None})
 
 @app.post("/result")
-def translate(request: Request, input_text):
-    map ={
-        "A": "а",
-        "Б": "b",
-        "В": "B",
-        "Г": "G",
-        "Д": "D",
-        "E": "e",
-        "Ж": "}|{",
-        "З": "3",
-        "И": "u",
-        "K": "K",
-        "Л": "J|",
-        "M": "M",
-        "H": "H",
-        "O": "O",
-        "П": "p",
-        "P": "P",
-        "C": "C",
-        "T": "T",
-        "У": "y",
-        "ф": "f"
+def translate(request: Request, input_text: str = Form(...)):
+    translated_text = cyrillic_to_latin(input_text)
+    return templates.TemplateResponse("html/index.html", {"request": request, "translated_text": translated_text})
+
+
+def cyrillic_to_latin(text):
+    transliteration = {
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
+        'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
+        'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+        'Ф': 'F', 'Х': 'H', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch',
+        'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya',
+
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e',
+        'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm',
+        'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u',
+        'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+        'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'
     }
-    translated_text = input_text
-    return templates.TemplateResponse("index.html", {"request": request, "translated_text": translated_text})
+
+    latin_text = "".join(transliteration.get(c, c) for c in text)
+
+    return latin_text
